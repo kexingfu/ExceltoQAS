de8dc04ecad36591b17d95b9ed4b4dc1c5d20b57
kexingfu <kexingfu5@gmail.com>
2018-08-25 17:40:42 +0800

    zanshi

---
+++ t0.1.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*- 
+import re
+
+f = open("d:\\liebiao.txt",encoding='utf-8-sig')
+line = f.readline()
+
+s="|"
+while line:
+    ttt = re.split(r'[\s]+',line.strip())
+
+    for wor in ttt:
+        s = s + "|" + wor 
+
+print(s)
+f.close()
