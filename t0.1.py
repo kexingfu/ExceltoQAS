fbaf458e9eef2a63e1f869e12b5506e8b3a42e08
kexingfu <kexingfu5@gmail.com>
2018-08-25 19:50:03 +0800

    v0.1版本

--- t0.1.py
+++ t0.1.py
@@ -4,15 +4,14 @@ import re
 f = open("d:\\liebiao.txt",encoding='utf-8-sig')
 line = f.readline()
 
-# s="|"
+print('|', end = '')
 while line:
-#     ttt = re.split(r'[\s]+',line.strip())
-    print(line, end = '')
+    ttt = re.split(r'[\s]+',line.strip())
+    # print(ttt, end = '')
+    s = '|'
+    for t in ttt:
+        s = s + '|' + t
+    print(s)    
     line = f.readline()
 
-
-#     for wor in ttt:
-#         s = s + "|" + wor 
-
-# print(s)
 f.close()
